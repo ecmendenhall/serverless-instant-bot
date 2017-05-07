ad309a25876252f8627d28cbfaeec786186067ad
Connor Mendenhall <ecmendenhall@gmail.com>
2017-05-06 22:39:52 -0400

    Package node_modules.

--- bot.js
+++ bot.js
@@ -1,13 +1,9 @@
 const instantBot = require('instant-bot');
 
-const run = () => {
+module.exports.run = () => {
 
   instantBot().then((bot) => {
-    bot.post('Hello world!');
+    bot.service.post('Hello world!');
   });
 
 };
-
-module.exports = {
-  run: run
-}
